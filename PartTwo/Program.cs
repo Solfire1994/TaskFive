1e3a66f9626785dcc35e06d9dd3f0fae60eddcb2
solfire1994 <solfire1994@gmail.com>
2022-08-02 00:06:33 +0300

    Add project files.

---
+++ PartTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PartTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string str = Console.ReadLine();
+            ReverseWords(str);
+            Console.ReadKey();
+        }
+
+        static void ReverseWords(string str)
+        {
+            string[] arr = DivideString(str);
+            string resultString = arr[^1];
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                resultString = string.Concat(resultString, " ", arr[i]);
+            }
+            Console.WriteLine(resultString);
+        }
+
+        static string[] DivideString(string str)
+        {
+            return str.Split(' ');
+        }
+
+    }
+}
---
+++ TaskFive/PartOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TaskFive
+{
+    internal class PartOne
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string str = Console.ReadLine();
+            Print(DivideString(str));
+            Console.ReadKey();
+        }
+
+        static string[] DivideString(string str)
+        {
+            return str.Split(' ');
+        }
+
+        static void Print(string[] arr)
+        {
+            Console.Clear();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
